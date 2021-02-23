aae022f66b5aaf706a23dc65b826f23408155c79
ravs788 <ravs788@gmail.com>
2021-02-23 14:38:55 +0530

    Add files via upload

---
+++ Cars/Audi.py
@@ -0,0 +1,10 @@
+# Python code to illustrate the Module 
+class Audi: 
+ 
+    def __init__(self): 
+        self.models = ['q7', 'a6', 'a8', 'a3'] 
+   
+    def outModels(self): 
+        print('These are the available models for Audi') 
+        for model in self.models: 
+            print('\t % s ' % model) 
